5917dee051dd65b8a4809333256366a9d86f3491
Hongjihoo123 <Hongjihoo123@naver.com>
2026-07-09 22:56:35 +0900

    as

--- Assets/HJH/Scripts/Boojick.cs
+++ Assets/HJH/Scripts/Boojick.cs
@@ -2,19 +2,25 @@ using UnityEngine;
 
 public class Boojick : MonoBehaviour
 {
-
     [SerializeField] private GameObject gasParticle;
     [SerializeField] private GameObject boojickParticle;
-
     [SerializeField] private GameObject butthole;
-    
+    [SerializeField] private Animator animator;
+
+    private static readonly int PoopTrigger = Animator.StringToHash("Poop");
+
+    public void PlayPoopSequence()
+    {
+        animator.SetTrigger(PoopTrigger);
+    }
+
     public void Gas()
     {
-        Instantiate(gasParticle, butthole.transform);
+        Instantiate(gasParticle, butthole.transform.position, Quaternion.identity);
     }
 
     public void Boojicks()
     {
-        Instantiate (boojickParticle, butthole.transform);
+        Instantiate(boojickParticle, butthole.transform.position, Quaternion.identity);
     }
 }
